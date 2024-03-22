a36566ce967e32b031b2c4d86b9b7fbe07f8de17
MaxDobr <maxim.dobrencko@rambler.ru>
2024-03-22 23:41:34 +0300

    main-min

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def plus(a, b):
-    print(a+b)
-def min(c, d):
-    print(c-d)
+from funcs import min
 
 first = int(input("Введите первое число: "))
 second = int(input("Введите второе числ: "))
 
-plus(first, second)
 min(first, second)
