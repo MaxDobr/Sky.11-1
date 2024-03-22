6e6c30583b2bc3d47a4d6e70f7508809e14187fd
MaxDobr <maxim.dobrencko@rambler.ru>
2024-03-22 23:31:47 +0300

    min

--- funcs.py
+++ funcs.py
@@ -1,7 +1,10 @@
 def plus(a, b):
     print(a+b)
+def min(c, d):
+    print(c-d)
 
 first = int(input("Введите первое число: "))
 second = int(input("Введите второе числ: "))
 
 plus(first, second)
+min(first, second)
